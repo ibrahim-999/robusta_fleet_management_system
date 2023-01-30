3c35c9bc53116903d63ccdc9ac2ecb2881da512c
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 09:59:14 +0200

    Created bus trips migration file & indexed bus_id

---
+++ database/migrations/2023_01_30_075311_create_bus_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Bus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus_trips', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('trip_start_date');
+            $table->dateTime('trip_end_date');
+            $table
+                ->foreignIdFor(Bus::class)
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus_trips');
+    }
+};
