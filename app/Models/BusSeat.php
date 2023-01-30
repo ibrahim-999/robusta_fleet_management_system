97530b1edefc1cacdc3a9bdffb3665a742872e78
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 11:22:12 +0200

    Related BusSeat to Booking model

--- app/Models/BusSeat.php
+++ app/Models/BusSeat.php
@@ -5,15 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BusSeat extends Model
 {
     use HasFactory;
+    
     protected $fillable = [
         'bus_id'
     ];
     public function bus(): BelongsTo
     {
         return $this->belongsTo(Bus::class);
+    }
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
     }
 }
