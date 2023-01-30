52aec7f6217cbc03ab1d69fc54b69ff4a9318020
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 09:51:24 +0200

    Initiated bus module

---
+++ database/seeders/BusSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bus;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bus::factroy(2)->create();
+    }
+}
