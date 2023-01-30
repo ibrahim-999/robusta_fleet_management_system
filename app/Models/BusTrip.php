966f2b529ce661243161754a20c1b8a0751c4010
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 10:56:35 +0200

    Related bus stations to bus trips

--- app/Models/BusTrip.php
+++ app/Models/BusTrip.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BusTrip extends Model
 {
@@ -22,5 +23,12 @@ class BusTrip extends Model
     public function bus(): BelongsTo
     {
         return $this->belongsTo(Bus::class);
+    }
+
+    public function stations(): HasMany
+    {
+        return $this
+            ->hasMany(BusTripStation::class)
+            ->orderBy('order');
     }
 }
