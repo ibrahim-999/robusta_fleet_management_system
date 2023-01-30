564c828da0efcc8ce428feffc93ff3a5d1cc8938
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 11:47:33 +0200

    Related BusTripStation to Booking Station model

--- app/Models/BusTripStation.php
+++ app/Models/BusTripStation.php
@@ -5,24 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BusTripStation extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'bus_ride_id',
+        'bus_trip_id',
         'city_id',
         'order'
     ];
-
     public function city(): BelongsTo
     {
         return $this->belongsTo(City::class);
     }
-
     public function busTrip(): BelongsTo
     {
         return $this->belongsTo(BusTrip::class);
+    }
+    public function bookingStations(): HasMany
+    {
+        return  $this->hasMany(BookingStation::class);
     }
 }
