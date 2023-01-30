7f03f07107bbcdbe6dce297dc8b9576f5b78c53d
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 11:27:07 +0200

    Reverse related Booking model relations

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,17 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Booking extends Model
 {
     use HasFactory;
-
     protected $fillable =[
-        'bus_ride_id',
+        'bus_trip_id',
         'user_id',
         'bus_seat_id',
         'start_station',
         'finish_station'
     ];
+
+    public function busSeat(): BelongsTo
+    {
+        return $this->belongsTo(BusSeat::class);
+    }
+    public function busTrip(): BelongsTo
+    {
+        return $this->belongsTo(BusTrip::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
