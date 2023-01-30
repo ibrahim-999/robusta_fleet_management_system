84979f298ef401a1fc7adcfc06b44041cef63835
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 10:41:42 +0200

    Migrated bus_seats with indexing bus_id

---
+++ database/migrations/2023_01_30_083623_create_bus_seats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Bus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus_seats', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignIdFor(Bus::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus_seats');
+    }
+};
