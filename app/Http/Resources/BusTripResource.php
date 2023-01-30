fe4d76a7a487571578e4be747b83211f86820023
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 12:21:13 +0200

    Created resources for bus and cities

---
+++ app/Http/Resources/BusTripResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BusTripResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'trip_start_date' => $this->trip_start_date->toDateTimeString(),
+            'trip_end_date' => $this->trip_end_date->toDateTimeString(),
+            'stations_count' => $this->stations_count,
+        ];
+    }
+}
