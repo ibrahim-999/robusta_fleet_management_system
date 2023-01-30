572ce932477edc6cf5e7dc2f8eba46939beb4709
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 13:29:04 +0200

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('v1')
+    ->group(function () {
+        Route::post('login', [LoginController::class, 'login']);
+
+        Route::middleware('auth:sanctum')->prefix('booking')
+            ->controller(BookingController::class)
+            ->group(function () {
+                Route::get('cities', 'getCities');
+                Route::get('available-seats', 'getAvailableSeats');
+                Route::post('book-ride', 'bookRide');
+            });
+
+    });
