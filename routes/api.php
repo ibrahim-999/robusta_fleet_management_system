d392f52ce8b0f11eb0060d5ca99d816993bc2d4c
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 13:48:38 +0200

    Apply lint

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::prefix('v1')
             ->group(function () {
                 Route::get('cities', 'getCities');
                 Route::get('available-seats', 'getAvailableSeats');
-                Route::post('book-ride', 'bookTrip');
+                Route::post('book-trip', 'bookTrip');
             });
 
     });
