e7c1b56cf54e1669149af5b618ace90f7910489b
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-30 12:39:37 +0200

    book trip function

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookTripRequest;
 use App\Http\Requests\GetAvailableSeatsRequest;
 use App\Http\Resources\BusTripResource;
 use App\Http\Resources\CitiesResource;
+use App\Models\BookingStation;
 use App\Models\BusTrip;
+use App\Models\BusTripStation;
 use App\Models\City;
 use App\Services\BookingService;
 use Illuminate\Http\JsonResponse;
@@ -27,12 +30,12 @@ class BookingController extends Controller
         ]);
     }
 
-    public function getAvailableSeats(GetAvailableSeatsRequest $request)
+    public function getAvailableSeats(GetAvailableSeatsRequest $getAvailableSeatsRequest)
     {
         $busy_seats = $this->bookingService
             ->setStartAndFinishStations(
-                $request->get('start_station'),
-                $request->get('finish_station'))
+                $getAvailableSeatsRequest->get('start_station'),
+                $getAvailableSeatsRequest->get('finish_station'))
             ->getBusySeats();
 
         $buses = BusTrip::whereDay('trip_start_date', today())
@@ -54,6 +57,41 @@ class BookingController extends Controller
             ->withCount('stations')
             ->paginate();
         return BusTripResource::collection($buses);
+    }
+
+    public function bookTrip(BookTripRequest $bookTripRequest)
+    {
+        $booking_data = collect($bookTripRequest->validated('seat_numbers'))
+            ->map(function ($seat_number) use ($bookTripRequest) {
+                return [
+                    'bus_trip_id' => $bookTripRequest->validated('ride_id'),
+                    'bus_seat_id' => $seat_number,
+                    'start_station' => $bookTripRequest->validated('start_station'),
+                    'finish_station' => $bookTripRequest->validated('finish_station'),
+                ];
+            })->toArray();
+        $bookings = $bookTripRequest->user()->bookings()
+            ->createMany($booking_data);
+        $stations = BusTripStation::whereBetween('id',
+            [
+                $bookTripRequest->validated('start_station'),
+                $bookTripRequest->validated('finish_station')
+            ])
+            ->orderBy('order')->get('id');
+        $booking_station_data = [];
+
+        foreach ($bookings as $booking) {
+            foreach ($stations as $station) {
+                $booking_station_data[] = [
+                    'bus_trip_station_id' => $station->id,
+                    'booking_id' => $booking->id,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ];
+            }
+        }
+        BookingStation::insert($booking_station_data);
+        return new JsonResponse;
     }
 
 }
